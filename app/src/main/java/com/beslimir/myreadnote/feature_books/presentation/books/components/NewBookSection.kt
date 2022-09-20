f55ce6afea347c17298ba986ff720e3165d40527
beslimir <marin.beslo@gmail.com>
2022-09-20 12:54:19 +0200

    add drop down menu for book type in new book section

--- app/src/main/java/com/beslimir/myreadnote/feature_books/presentation/books/components/NewBookSection.kt
+++ app/src/main/java/com/beslimir/myreadnote/feature_books/presentation/books/components/NewBookSection.kt
@@ -5,17 +5,21 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.beslimir.myreadnote.feature_books.presentation.books.BooksViewModel
+import com.beslimir.myreadnote.feature_books.util.BookType
 import com.beslimir.myreadnote.feature_books.util.BooksEvent
 import com.beslimir.myreadnote.ui.theme.DarkGray
 
@@ -87,22 +91,7 @@ fun NewBookSection(
                     viewModel.onEvent(BooksEvent.ClearAuthorInput)
                 }
             )
-            BookSectionItem(
-                header = bookTypeState.header,
-                inputValue = bookTypeState.inputValue,
-                hint = bookTypeState.hint,
-                textStyle = MaterialTheme.typography.h6,
-                isHintVisible = bookTypeState.isHintVisible,
-                onFocusChange = {
-                    viewModel.onEvent(BooksEvent.ChangeBookTypeFocus(it))
-                },
-                onValueChange = {
-                    viewModel.onEvent(BooksEvent.EnteredBookType(it))
-                },
-                onClearInput = {
-
-                }
-            )
+            DropdownDemo(textStyle = MaterialTheme.typography.h6)
             Row(
                 modifier = Modifier
                     .fillMaxSize()
@@ -128,6 +117,118 @@ fun NewBookSection(
                     Text(
                         text = "Save",
                         color = Color.Gray
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DropdownDemo(textStyle: TextStyle) {
+    var expanded by remember { mutableStateOf(false) }
+    val dropDownItems = BookType.values().asList()
+    var dropDownIcon by remember { mutableStateOf(Icons.Default.ArrowDropDown) }
+    var selectedIndex by remember { mutableStateOf(0) }
+    val grayTextStyle = textStyle.copy(color = Color.Gray)
+
+    Text(
+        text = "Book type",
+        fontSize = 12.sp,
+        color = Color.DarkGray,
+        modifier = Modifier
+            .padding(
+                start = 16.dp,
+                end = 16.dp,
+                top = 16.dp,
+                bottom = 4.dp
+            )
+    )
+    Column(
+        modifier = Modifier
+            .wrapContentSize(Alignment.TopStart)
+            .padding(
+                start = 16.dp,
+                end = 16.dp,
+                bottom = 8.dp
+            )
+            .clip(RoundedCornerShape(5.dp))
+            .shadow(
+                elevation = 8.dp,
+                shape = RoundedCornerShape(5.dp)
+            )
+            .background(MaterialTheme.colors.surface)
+            .clickable {
+                dropDownIcon = Icons.Default.ArrowDropUp
+                expanded = true
+            }
+    ) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = dropDownItems[selectedIndex].name
+                    .lowercase()
+                    .replaceFirstChar {
+                        it.uppercase()
+                    },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 8.dp,
+                        end = 64.dp,
+                        top = 4.dp,
+                        bottom = 4.dp
+                    )
+                    .background(
+                        MaterialTheme.colors.surface
+                    ),
+                style = grayTextStyle,
+                maxLines = 1
+            )
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .height(400.dp)
+                    .wrapContentWidth()
+                    .background(Color.LightGray)
+            ) {
+                dropDownItems.forEachIndexed { index, s ->
+                    DropdownMenuItem(onClick = {
+                        selectedIndex = index
+                        expanded = false
+                        dropDownIcon = Icons.Default.ArrowDropDown
+                    }) {
+                        Text(
+                            text = s.name
+                                .lowercase()
+                                .replaceFirstChar {
+                                    it.uppercase()
+                                },
+                            style = grayTextStyle,
+                            maxLines = 1
+                        )
+                    }
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 4.dp, end = 4.dp),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                IconButton(
+                    onClick = {
+                        dropDownIcon = Icons.Default.ArrowDropUp
+                        expanded = true
+                    },
+                    modifier = Modifier.size(20.dp)
+                ) {
+                    Icon(
+                        imageVector = dropDownIcon,
+                        contentDescription = "Open DropDown",
+                        tint = Color.Gray
                     )
                 }
             }
