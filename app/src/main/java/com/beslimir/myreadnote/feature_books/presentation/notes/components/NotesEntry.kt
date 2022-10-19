9187d1306da7927476c8d1e1f8ff64e85c9d8a29
beslimir <marin.beslo@gmail.com>
2022-10-19 14:53:17 +0200

    Align notes title and content

--- app/src/main/java/com/beslimir/myreadnote/feature_books/presentation/notes/components/NotesEntry.kt
+++ app/src/main/java/com/beslimir/myreadnote/feature_books/presentation/notes/components/NotesEntry.kt
@@ -2,10 +2,9 @@ package com.beslimir.myreadnote.feature_books.presentation.notes.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -46,15 +47,29 @@ fun NotesEntry(
             },
         contentAlignment = Alignment.Center
     ) {
-        Column {
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
                 text = entry.notes[index].noteTitle ?: "No title",
+                style = MaterialTheme.typography.h6,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color.DarkGray,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
             Text(
                 text = entry.notes[index].noteContent,
-                maxLines = 5
+                style = MaterialTheme.typography.body2,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Medium,
+                color = Color.DarkGray,
+                maxLines = 5,
+                overflow = TextOverflow.Ellipsis
             )
         }
     }
